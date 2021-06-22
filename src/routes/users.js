9834b82598a18d2b5ae622f9d744b63f7d64b6fe
Johar Terraza <you@example.comjoharterraza211199@gmail.com>
2021-06-21 19:29:29 -0700

    Primer commit

---
+++ src/routes/users.js
@@ -0,0 +1,19 @@
+  
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection  = require('../database.js');
+
+// GET all Employees
+router.get('/users', (req, res) => {
+  mysqlConnection.query(`select * from usuarios as u
+  join direcciones as d on d.idDirecciones = u.fkDireccion`, (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });  
+});
+
+module.exports = router;
